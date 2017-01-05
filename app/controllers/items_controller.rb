578dba67271bef669cb83d9f4320c4905f478c25
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-01-05 12:30:38 -0700

    see all items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
   def index
-    @category = Category.find(params[:category_id])
-    @items = @category.find_items_associated_with_category
+    if request.original_fullpath == '/items'
+      @items = Item.all
+    else
+      @category = Category.find(params[:category_id])
+      @items = @category.find_items_associated_with_category
+    end
   end
 end
