f5761b8329a081a8c3001da75c0010622596f13e
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-01-07 16:08:41 -0700

    remove ransack

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,8 @@
 class CategoriesController < ApplicationController
   def index
-    @q = Category.ransack(params[:q])
-    @category = @q.result(distinct: true)
-
     @cart_items = cart.items
     @categories = Category.all
   end
 
-  
+
 end
