96945dd48d6309a455f8ac6554675fa7f0fda28d
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-01-07 11:35:23 -0700

    finish functionality for user authentication, login, logout

--- spec/features/users/user_can_be_created_spec.rb
+++ spec/features/users/user_can_be_created_spec.rb
@@ -11,12 +11,12 @@ describe "Visitor can create an account" do
 
     expect(current_path).to eq(new_user_path)
     email    = "sample@sample.com"
-    password = "1234"
+    password = "1234567"
     name     = "John Smith"
-    fill_in "Name", with: name
-    fill_in "Email", with: email
-    fill_in "Password", with: password
-    fill_in "Confirm Password", with: password
+    fill_in "user[name]", with: name
+    fill_in "user[email]", with: email
+    fill_in "user[password]", with: password
+    fill_in "user[password_confirmation]", with: password
     click_button "Create Account!"
 
     expect(current_path).to eq(root_path)
