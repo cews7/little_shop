2fbf726124647f03b3c79752cddf93aad19d4649
Marisa Burton <marisamburton@gmail.com>
2017-01-06 20:15:47 -0700

    completes all user model testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, :email, :password_digest, presence: true
-  validates :password_digest, confirmation: true
+  validates :password_digest, confirmation: true, length: { minimum: 6 }, allow_nil: true
   validates :email, uniqueness: true
-  validates :password_digest, presence: true, length: { minimum: 6 }, allow_nil: true
 end
