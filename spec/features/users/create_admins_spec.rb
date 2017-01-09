d4605f0dd5290ad209c5bc96a53e33ad894e603d
Marisa Burton <marisamburton@gmail.com>
2017-01-09 16:06:06 -0700

    some route changes

--- spec/features/users/create_admins_spec.rb
+++ spec/features/users/create_admins_spec.rb
@@ -6,7 +6,7 @@ describe "Admin visits the admin page" do
       admin = User.create!(name: "John Smith", email: "jo@jo.com", password: "1234567", password_confirmation: "1234567", role: 1)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(:admin)
-      visit admin_home_index_path
+      visit admin_dashboard_index_path
       expect(page).to have_content("Admin Page")
     end
   end
@@ -15,7 +15,7 @@ describe "Admin visits the admin page" do
       user = User.create(name: "John Smith", email: "sample2@sample.com", password_digest: "1234567", password_digest_confirmation: "1234567", role: 0)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(:default)
-      visit admin_home_index_path
+      visit admin_dashboard_index_path
       expect(page).to_not have_content("Admin Page")
       expect(page).to have_content("You are being redirected")
     end
