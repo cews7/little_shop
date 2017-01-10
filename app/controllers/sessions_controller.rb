3cdd695d9e8240543f7ed637213f5e1085eca6bb
Marisa Burton <marisamburton@gmail.com>
2017-01-09 18:11:55 -0700

    finishes 16 & 17 wo tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,12 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:session][:email])
+    session[:user_id] = @user.id
     if @user && @user.authenticate(params[:session][:password]) && current_admin
-      session[:user_id] = @user.id
       redirect_to admin_dashboard_index_path
       flash[:success] = "Admin successfully logged in!"
     elsif @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
-      redirect_to admin_dashboard_index_path
+      redirect_to dashboard_path
       flash[:success] = "Successfully logged in!"
     else
       redirect_to login_path
