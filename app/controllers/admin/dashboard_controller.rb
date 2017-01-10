7475074a4232d7e7e81307e0ad4f4fa0d568233c
Marisa Burton <marisamburton@gmail.com>
2017-01-10 13:25:08 -0700

    admin edit page working

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,7 +3,29 @@ class Admin::DashboardController < ApplicationController
 
   def index
     @items = Item.all
-
     @cart_items = cart.items
   end
+
+  def edit
+    @user = User.find(params[:id])
+    @cart_items = cart.items
+
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_dashboard_index_path
+      flash[:success] = "Admin profile successfully updated!"
+    else
+      render :edit
+      flash[:danger] = "Admin profile has NOT been updated!"
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
