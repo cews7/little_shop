556eea9689c829e96376837d2de3ffbf78f90878
Jason Conrad <jdconrad@liberty.edu>
2017-01-12 08:10:19 -0700

    add email tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
+  config.action_mailer.delivery_method     = :test
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
