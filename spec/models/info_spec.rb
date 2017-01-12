b26879fb9bce77d1a4b3bbe4d78d6a4b6cd18721
Marisa Burton <marisamburton@gmail.com>
2017-01-11 20:11:10 -0700

    info model 1 test passing

--- spec/models/info_spec.rb
+++ spec/models/info_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Info, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes when creating user info" do
+      it "is invalid without a name" do
+        info = Info.create(address: "11 Sample Way", city: "Sample", state: "CO", postal_code: "80238", country: "USA")
+
+        expect(info).to be_invalid
+      end
+    end
+  end
 end
