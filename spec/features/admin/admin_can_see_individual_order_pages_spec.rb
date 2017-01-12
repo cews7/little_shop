344059b12d1a650f700211358822a75527c4d7fa
Marisa Burton <marisamburton@gmail.com>
2017-01-11 20:03:20 -0700

    admin order test 3 passing

--- spec/features/admin/admin_can_see_individual_order_pages_spec.rb
+++ spec/features/admin/admin_can_see_individual_order_pages_spec.rb
@@ -59,9 +59,8 @@ describe "Admin visits the admin individual order page" do
       expect(page).to have_content("2")
       expect(page).to have_content("Subtotal")
     end
-  end
 
-  context "as admin do" do
+
     it "allows admin to see cancel an order" do
       visit root_path
 
@@ -76,7 +75,7 @@ describe "Admin visits the admin individual order page" do
       expect(page).to_not have_content("You are being redirected")
 
       click_link "View Order"
-      save_and_open_page
+
       expect(current_path).to eq(admin_order_path(@order_1.id))
 
       click_button "Cancel"
@@ -86,8 +85,37 @@ describe "Admin visits the admin individual order page" do
       expect(page).to have_content("Order Status: Canceled")
 
       click_link "Back to Admin Dashboard"
-      save_and_open_page
       expect(current_path).to eq(admin_dashboard_index_path)
+    end
+
+  it "allows admin to mark order as paid" do
+      visit root_path
+
+      click_link "Log In | Sign Up"
+
+      fill_in :session_email, with: "john@smith.com"
+      fill_in :session_password, with: "1234567"
+
+      click_button "Login"
+
+      expect(page).to have_content("Admin Dashboard")
+      expect(page).to_not have_content("You are being redirected")
+
+      click_link "View Order"
+
+      expect(current_path).to eq(admin_order_path(@order_1.id))
+
+      click_button "Mark as Paid"
+
+      expect(current_path).to eq(admin_order_path(@order_1.id))
+      expect(page).to have_content("Order Status: Paid")
+      expect(page).to have_button("Completed")
+
+      click_button "Completed"
+
+      expect(current_path).to eq(admin_order_path(@order_1.id))
+      expect(page).to have_content("Order has been Completed")
+      expect(page).to have_content("Order Status: Completed")
     end
   end
 end
